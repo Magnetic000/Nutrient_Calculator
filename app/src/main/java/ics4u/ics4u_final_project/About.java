81ee347781c97245c3465892e758f6ab72680de0
Isaac Wismer <idw.wismer@gmail.com>
2016-01-19 20:32:44 -0500

    added about

--- app/src/main/java/ics4u/ics4u_final_project/About.java
+++ app/src/main/java/ics4u/ics4u_final_project/About.java
@@ -1,7 +1,33 @@
+/*
+Copyright (C) 2016 Isaac Wismer & Andrew Xu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ics4u.ics4u_final_project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Andrew on 2016-01-19.
@@ -11,5 +37,19 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+        BufferedReader br = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.about)));
+        String file = "";
+        try {
+            //read each line of the file and add it to the arraylist
+            String s = br.readLine();
+            while (s != null) {
+                file += s + "\n";
+                s = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        TextView about = (TextView) findViewById(R.id.about);
+        about.setText(Html.fromHtml(file));
     }
 }
